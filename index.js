a46982ade386cf0b5cadfa6764706d9c8572df04
Guilhem <guilhem.mazoyer@laposte.net>
2025-10-20 22:42:06 +0900

    Add the port open

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import { Client, GatewayIntentBits, Partials } from "discord.js";
 import dotenv from "dotenv";
+import http from "http";
 dotenv.config();
 
 const TOKEN = process.env.DISCORD_TOKEN; // token dans le .env
+const PORT = process.env.PORT || 3000;
+const SELF_URL = process.env.RENDER_EXTERNAL_URL; // fourni automatiquement par Render
+
+// Petit serveur HTTP
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("✅ Bot online\n");
+}).listen(PORT, () => console.log(`Open on port ${PORT}`));
 
 // --- Initialisation du client Discord ---
 const client = new Client({
@@ -14,6 +23,7 @@ const client = new Client({
   ],
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
+
 
 // --- Quand le bot est prêt ---
 client.once("ready", () => {
