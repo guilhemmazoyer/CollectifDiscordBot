390699494a8fed4b18d4775b17e51eecbe398fbb
Guilhem <guilhem.mazoyer@laposte.net>
2025-10-20 22:32:00 +0900

    fix bot intent

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-import { Client } from 'discord.js';
-import dotenv from 'dotenv';
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+import dotenv from "dotenv";
 dotenv.config();
 
 const TOKEN = process.env.DISCORD_TOKEN; // token dans le .env
 
-const client = new Client();
+// --- Initialisation du client Discord ---
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
 
 // --- Quand le bot est prêt ---
 client.once("ready", () => {
