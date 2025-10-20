be25061f0b484b713bc51675a9c131f90825792c
Guilhem <guilhem.mazoyer@laposte.net>
2025-10-20 22:28:54 +0900

    retrieve missing file for the bot to work even if it's empty

---
+++ config.js
@@ -0,0 +1,26 @@
+export default {
+  roles: {
+    admin: "1425368300594008205",
+    creator: "1425368331417944185",
+    member: "1425110795741827196",
+  },
+  categories: {
+    creatorProjects: "1425430472145637437", // catégorie des projets
+  },
+  channels: {
+    administration: "1425438654620373045", // salon de modération
+    billboardAffiliation: "1425368036214571058",
+    billboardTicket: "1425436541882339369",
+  },
+  messages: {
+    chart: "1425368514918879272",
+  },
+  emojis: {
+    validate: "✅",
+  },
+};
+
+
+import config from "./config.js";
+
+const ADMIN_ROLE_ID = config.roles.admin;
